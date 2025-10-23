334d54e4663a8909839018f0baad69322ea2aad0
lcaparada <lcaparada@gmail.com>
2025-10-23 10:54:41 -0300

    feat

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,13 +1,9 @@
-// Export controllers
 export { ProfileController } from "./profile.controller";
 
-// Import services from container
 import { serviceContainer } from "../container";
 
-// Import controllers
 import { ProfileController } from "./profile.controller";
 
-// Initialize controllers with injected dependencies
 export const profileController = new ProfileController(
   serviceContainer.metadataService,
   serviceContainer.storageService,
